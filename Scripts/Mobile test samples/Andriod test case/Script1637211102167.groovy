50ea0bcab27bf84c3b61eb5a1b357f5b5da4c2b3
AekachaiBoonruang <sniperaek@gmail.com>
2021-11-18 15:24:32 +0700

    First commit

---
+++ Scripts/Mobile test samples/Andriod test case/Script1637211102167.groovy
@@ -0,0 +1,36 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.appium.driver.AppiumDriverManager
+import io.appium.java_client.android.AndroidDriver
+import org.openqa.selenium.remote.DesiredCapabilities
+import com.kms.katalon.core.mobile.driver.MobileDriverType
+// Set the Browserstack credentials: USERNAME and ACCESS_KEY
+String browserStackServerURL = "Get remote url from browser stack";
+
+DesiredCapabilities capabilities = new DesiredCapabilities();
+
+capabilities.setCapability("device", "Google Pixel 3");
+
+//Set the app_url (returned on uploading app on Browserstack) in the 'app' capability
+capabilities.setCapability('app', 'app_url (returned on uploading app on Browserstack)');
+
+AppiumDriverManager.createMobileDriver(MobileDriverType.ANDROID_DRIVER, capabilities, new URL(browserStackServerURL));
+
+Mobile.takeScreenshot('Screenshot', FailureHandling.STOP_ON_FAILURE)
+
